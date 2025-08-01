8ee2a1abd2ac5672081108c1b27002d1b32c084b
Gustavo Batistela <gustavoabatistela@gmail.com>
2025-07-31 21:50:24 -0300

    feat: validation module

--- matcher/src/lib.rs
+++ matcher/src/lib.rs
@@ -1,1 +1,215 @@
 mod size_hint;
+mod validation;
+
+use crate::size_hint::CalculateSizeHint;
+use crate::size_hint::SizeHintState;
+use regex::Regex;
+use std::collections::HashSet;
+use validation::Validation;
+
+#[derive(Clone, Debug, Hash, Eq, PartialEq)]
+pub struct RegexMatch<'a> {
+    pub original: &'a str,
+    pub span: SourceSpan,
+    pub captured_groups: Vec<Option<SourceSpan>>,
+}
+
+impl<'a> RegexMatch<'a> {
+    pub fn new(
+        original: &'a str,
+        span: SourceSpan,
+        captured_groups: Vec<Option<SourceSpan>>,
+    ) -> Self {
+        Self {
+            original,
+            span,
+            captured_groups,
+        }
+    }
+
+    pub fn advance_end_by(self, n: usize) -> Self {
+        debug_assert!(self.span.end + n <= self.original.len());
+        Self {
+            original: self.original,
+            span: SourceSpan {
+                start: self.span.start,
+                end: self.span.end + n,
+            },
+            captured_groups: self.captured_groups,
+        }
+    }
+}
+
+impl<'a> From<&RegexMatch<'a>> for HashSet<RegexMatch<'a>> {
+    fn from(value: &RegexMatch<'a>) -> Self {
+        HashSet::from([value.clone()])
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Matcher {
+    regex: Regex,
+    input: String,
+}
+
+impl Matcher {
+    pub fn new(regex: Regex, input: String) -> Self {
+        Self { regex, input }
+    }
+
+    pub fn solve(&self) -> HashSet<RegexMatch> {
+        let search_space = self.gen_search_space();
+        let mut matches = HashSet::new();
+        for span in search_space {
+            let input = span.substr(&self.input);
+            let initial_match = RegexMatch::new(input, SourceSpan::default(), vec![]);
+            let res = self.regex.validate(&initial_match);
+            matches.extend(res.into_iter());
+        }
+        matches
+    }
+
+    fn gen_search_space(&self) -> Vec<SourceSpan> {
+        if self.input.is_empty() {
+            return vec![];
+        }
+
+        let char_count = self.input.chars().count();
+
+        let starts = if self.regex.start_anchor {
+            0..1
+        } else {
+            0..char_count
+        };
+
+        let mut state = SizeHintState::default();
+        let size_hint = self.regex.size_hint(&mut state);
+
+        if self.regex.end_anchor {
+            return starts
+                .filter_map(|start| {
+                    let end = char_count;
+                    let len = end - start;
+                    if size_hint.is_compatible(len) {
+                        Some(SourceSpan { start, end })
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+        }
+
+        let mut search_space = Vec::new();
+        for start in starts {
+            for end in start + 1..=char_count {
+                let len = end - start;
+                if size_hint.is_compatible(len) {
+                    search_space.push(SourceSpan { start, end });
+                }
+            }
+        }
+        search_space
+    }
+}
+
+#[derive(Clone, Debug, Default, Hash, Eq, PartialEq)]
+pub struct SourceSpan {
+    pub start: usize,
+    pub end: usize,
+}
+
+impl SourceSpan {
+    pub fn new(start: usize, end: usize) -> Self {
+        debug_assert!(start <= end);
+        SourceSpan { start, end }
+    }
+
+    pub fn start_byte(&self, s: &str) -> usize {
+        s.char_indices()
+            .nth(self.start)
+            .map(|(i, _)| i)
+            .unwrap_or(s.len())
+    }
+
+    pub fn end_byte(&self, s: &str) -> usize {
+        s.char_indices()
+            .nth(self.end)
+            .map(|(i, _)| i)
+            .unwrap_or(s.len())
+    }
+
+    pub fn substr<'a>(&self, s: &'a str) -> &'a str {
+        // This has to be done carefully to handle Unicode characters correctly.
+        &s[self.start_byte(s)..self.end_byte(s)]
+    }
+
+    pub fn intersects(&self, other: &SourceSpan) -> bool {
+        self.end > other.start && other.end > self.start
+    }
+
+    pub fn is_adjacent(&self, other: &SourceSpan) -> bool {
+        self.end == other.start || other.end == self.start
+    }
+
+    pub fn merge(&self, other: &SourceSpan) -> Option<SourceSpan> {
+        if self.intersects(other) || self.is_adjacent(other) {
+            Some(SourceSpan {
+                start: self.start.min(other.start),
+                end: self.end.max(other.end),
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.end - self.start
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.start == self.end
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case((0, 5), (4, 10), true; "point intersection")]
+    #[test_case((0, 5), (5, 10), false; "disjoint adjacent")]
+    #[test_case((0, 5), (6, 10), false; "disjoint distant")]
+    #[test_case((0, 5), (3, 6), true; "overlap")]
+    #[test_case((0, 5), (2, 3), true; "b contained in a")]
+    fn test_intersects(a: (usize, usize), b: (usize, usize), expected: bool) {
+        let a = SourceSpan::new(a.0, a.1);
+        let b = SourceSpan::new(b.0, b.1);
+        assert_eq!(a.intersects(&b), expected);
+        assert_eq!(b.intersects(&a), expected);
+    }
+
+    #[test_case((0, 5), (4, 10), false; "point intersection")]
+    #[test_case((0, 5), (5, 10), true; "disjoint adjacent")]
+    #[test_case((0, 5), (6, 10), false; "disjoint distant")]
+    #[test_case((0, 5), (3, 6), false; "overlap")]
+    #[test_case((0, 5), (2, 3), false; "b contained in a")]
+    fn test_is_adjacent(a: (usize, usize), b: (usize, usize), expected: bool) {
+        let a = SourceSpan::new(a.0, a.1);
+        let b = SourceSpan::new(b.0, b.1);
+        assert_eq!(a.is_adjacent(&b), expected);
+        assert_eq!(b.is_adjacent(&a), expected);
+    }
+
+    #[test_case((0, 5), (4, 10), Some((0, 10)); "point intersection")]
+    #[test_case((0, 5), (5, 10), Some((0, 10)); "disjoint adjacent")]
+    #[test_case((0, 5), (6, 10), None; "disjoint distant")]
+    #[test_case((0, 5), (3, 6), Some((0, 6)); "overlap")]
+    #[test_case((0, 5), (2, 3), Some((0, 5)); "b contained in a")]
+    fn test_merge(a: (usize, usize), b: (usize, usize), res: Option<(usize, usize)>) {
+        let a = SourceSpan::new(a.0, a.1);
+        let b = SourceSpan::new(b.0, b.1);
+        let res = res.map(|(start, end)| SourceSpan::new(start, end));
+        assert_eq!(a.merge(&b), res);
+        assert_eq!(b.merge(&a), res);
+    }
+}
---
+++ matcher/src/validation.rs
@@ -0,0 +1,374 @@
+use crate::RegexMatch;
+use crate::SourceSpan;
+use regex::Atom;
+use regex::CharClass;
+use regex::CharRange;
+use regex::Expression;
+use regex::Factor;
+use regex::Regex;
+use regex::Term;
+use std::collections::HashSet;
+
+pub trait Validation {
+    fn validate<'a>(&self, old: &RegexMatch<'a>) -> HashSet<RegexMatch<'a>>;
+
+    fn validate_all<'a>(&self, matches: HashSet<RegexMatch<'a>>) -> HashSet<RegexMatch<'a>> {
+        matches
+            .iter()
+            .flat_map(|old_match| self.validate(old_match))
+            .collect()
+    }
+}
+
+impl Validation for Regex {
+    fn validate<'a>(&self, initial: &RegexMatch<'a>) -> HashSet<RegexMatch<'a>> {
+        let mut res = self.expression.validate(initial);
+        if self.start_anchor {
+            res.retain(|m| m.span.start == 0);
+        }
+        if self.end_anchor {
+            res.retain(|m| m.span.end == initial.original.len());
+        }
+        res
+    }
+}
+
+impl Validation for Expression {
+    fn validate<'a>(&self, old: &RegexMatch<'a>) -> HashSet<RegexMatch<'a>> {
+        match self {
+            Expression::Term(term) => term.validate(old),
+            Expression::Alternation(term, expr) => {
+                let mut new = term.validate(old);
+                new.extend(expr.validate(old));
+                new
+            }
+        }
+    }
+}
+
+impl Validation for Term {
+    fn validate<'a>(&self, old: &RegexMatch<'a>) -> HashSet<RegexMatch<'a>> {
+        match self {
+            Term::Factor(factor) => factor.validate(old),
+            Term::Concatenation(factor, term) => term.validate_all(factor.validate(old)),
+        }
+    }
+}
+
+impl Validation for Factor {
+    fn validate<'a>(&self, old: &RegexMatch<'a>) -> HashSet<RegexMatch<'a>> {
+        match self {
+            Factor::Atom(atom) => atom.validate(old),
+            Factor::ZeroOrOne(atom) => {
+                let mut new = atom.validate(old);
+                new.insert(old.clone());
+                new
+            }
+            Factor::ZeroOrMore(atom) => {
+                let mut new = HashSet::new();
+                let mut current_input = HashSet::from([old.clone()]);
+                while !current_input.is_empty() {
+                    current_input = atom.validate_all(current_input);
+                    new.extend(current_input.clone());
+                }
+                new.insert(old.clone());
+                new
+            }
+            Factor::OneOrMore(atom) => {
+                let mut new = HashSet::new();
+                let mut current_input = HashSet::from([old.clone()]);
+                while !current_input.is_empty() {
+                    current_input = atom.validate_all(current_input);
+                    new.extend(current_input.clone());
+                }
+                new
+            }
+        }
+    }
+}
+
+impl Validation for Atom {
+    fn validate<'a>(&self, old: &RegexMatch<'a>) -> HashSet<RegexMatch<'a>> {
+        let mut new = HashSet::new();
+        let Some(next_char) = old.original.chars().nth(old.span.end) else {
+            return new;
+        };
+        match self {
+            Atom::Char(c) => {
+                if next_char == *c {
+                    new.insert(old.clone().advance_end_by(1));
+                }
+            }
+            Atom::Parentheses(expr) => {
+                // TODO improve
+                let n_entries = old.captured_groups.len();
+                let mut old_clone = old.clone();
+                old_clone.captured_groups.push(None);
+                let new_ref = expr.validate(&old_clone);
+                for mut new_match in new_ref.into_iter() {
+                    let new_start = old.span.end;
+                    let new_end = new_match.span.end;
+                    new_match.captured_groups[n_entries] =
+                        Some(SourceSpan::new(new_start, new_end));
+                    new.insert(new_match);
+                }
+            }
+            Atom::NormalClass(class) => {
+                new.extend(class.validate(old));
+            }
+            Atom::NegatedClass(class) => {
+                if class.validate(old).is_empty() {
+                    new.insert(old.clone().advance_end_by(1));
+                }
+            }
+            Atom::BackReference(n) => {
+                // TODO improve
+                if let Some(group) = &old.captured_groups[*n - 1] {
+                    let captured = group.substr(old.original);
+                    let end_byte = old.span.end_byte(old.original);
+                    let new_end_byte = old
+                        .original
+                        .char_indices()
+                        .nth(old.span.end + group.len())
+                        .map(|(i, _)| i)
+                        .unwrap_or(old.original.len());
+                    let rest = &old.original[end_byte..new_end_byte];
+                    if rest == captured {
+                        new.insert(old.clone().advance_end_by(captured.len()));
+                    }
+                } else {
+                    todo!("HAHA")
+                }
+            }
+        }
+        new
+    }
+}
+
+impl Validation for CharClass {
+    fn validate<'a>(&self, old: &RegexMatch<'a>) -> HashSet<RegexMatch<'a>> {
+        let mut new = HashSet::new();
+        let Some(next_char) = old.original.chars().nth(old.span.end) else {
+            return new;
+        };
+        match self {
+            CharClass::CharRange(range) => new.extend(range.validate(old)),
+            CharClass::CharRangeClass(range, class) => {
+                new.extend(range.validate(old));
+                new.extend(class.validate(old));
+            }
+            CharClass::WordChar => {
+                if next_char.is_ascii_alphanumeric() || next_char == '_' {
+                    new.insert(old.clone().advance_end_by(1));
+                }
+            }
+            CharClass::Digit => {
+                if next_char.is_ascii_digit() {
+                    new.insert(old.clone().advance_end_by(1));
+                }
+            }
+            CharClass::AnyChar => {
+                new.insert(old.clone().advance_end_by(1));
+            }
+        }
+        new
+    }
+}
+
+impl Validation for CharRange {
+    fn validate<'a>(&self, old: &RegexMatch<'a>) -> HashSet<RegexMatch<'a>> {
+        let mut new = HashSet::new();
+        let Some(next_char) = old.original.chars().nth(old.span.end) else {
+            return new;
+        };
+        match self {
+            CharRange::Char(c) => {
+                if next_char == *c {
+                    new.insert(old.clone().advance_end_by(1));
+                }
+            }
+            CharRange::CharRange(start, end) => {
+                if next_char >= *start && next_char <= *end {
+                    new.insert(old.clone().advance_end_by(1));
+                }
+            }
+        }
+        new
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Matcher;
+    use regex::Regex;
+    use std::str::FromStr;
+
+    fn is_match(regex: &Regex, input: &str) -> bool {
+        let matcher = Matcher::new(regex.clone(), input.to_string());
+        !matcher.solve().is_empty()
+    }
+
+    #[test]
+    fn test_char_class_validation() {
+        let regex = Regex::from_str("abc").unwrap();
+        assert!(is_match(&regex, "abc"));
+        assert!(is_match(&regex, "abcabc"));
+        assert!(!is_match(&regex, "aba"));
+        assert!(!is_match(&regex, "123"));
+        assert!(!is_match(&regex, ""));
+        assert!(!is_match(&regex, " "));
+        assert!(!is_match(&regex, "$!_"));
+    }
+
+    #[test]
+    fn test_digit_validation() {
+        let regex = Regex::from_str("\\d").unwrap();
+        assert!(is_match(&regex, "1"));
+        assert!(is_match(&regex, "123"));
+        assert!(is_match(&regex, "ab1abc"));
+        assert!(!is_match(&regex, ""));
+        assert!(!is_match(&regex, " "));
+        assert!(!is_match(&regex, "$!_"));
+        assert!(!is_match(&regex, "a"));
+    }
+
+    #[test]
+    fn test_alpha_numeric_validation() {
+        let regex = Regex::from_str("\\w").unwrap();
+        assert!(is_match(&regex, "1"));
+        assert!(is_match(&regex, "a"));
+        assert!(!is_match(&regex, ""));
+        assert!(!is_match(&regex, "$!-"));
+    }
+
+    #[test]
+    fn test_positive_char_group() {
+        let regex = Regex::from_str("[abc]").unwrap();
+        assert!(is_match(&regex, "a"));
+        assert!(is_match(&regex, "b"));
+        assert!(is_match(&regex, "c"));
+        assert!(is_match(&regex, "ab"));
+        assert!(is_match(&regex, "da"));
+        assert!(!is_match(&regex, "d"));
+        assert!(!is_match(&regex, ""));
+        assert!(!is_match(&regex, " "));
+        assert!(!is_match(&regex, "$![]"));
+        assert!(!is_match(&regex, "1"));
+    }
+
+    #[test]
+    fn test_negative_char_group() {
+        let regex = Regex::from_str("[^abc]").unwrap();
+        assert!(!is_match(&regex, "a"));
+        assert!(!is_match(&regex, "b"));
+        assert!(!is_match(&regex, "c"));
+        assert!(!is_match(&regex, "ab"));
+        assert!(is_match(&regex, "da"));
+        assert!(is_match(&regex, "def"));
+        assert!(!is_match(&regex, ""));
+        assert!(is_match(&regex, " "));
+        assert!(is_match(&regex, "$![]"));
+        assert!(is_match(&regex, "1"));
+    }
+
+    #[test]
+    fn test_start_anchor() {
+        let regex = Regex::from_str("^abc").unwrap();
+        assert!(is_match(&regex, "abc"));
+        assert!(is_match(&regex, "abcc"));
+        assert!(!is_match(&regex, "aabc"));
+        assert!(!is_match(&regex, "^abc"));
+    }
+
+    #[test]
+    fn test_end_anchor() {
+        let regex = Regex::from_str("abc$").unwrap();
+        assert!(is_match(&regex, "abc"));
+        assert!(is_match(&regex, "aabc"));
+        assert!(!is_match(&regex, "abcc"));
+        assert!(!is_match(&regex, "abc$"));
+    }
+
+    #[test]
+    fn test_one_or_more() {
+        let regex = Regex::from_str("ab+").unwrap();
+        assert!(is_match(&regex, "ab"));
+        assert!(is_match(&regex, "abbbbb"));
+        assert!(!is_match(&regex, "cbb"));
+
+        let regex = Regex::from_str("[^x]+").unwrap();
+        assert!(is_match(&regex, "a"));
+        assert!(is_match(&regex, "ab"));
+        assert!(!is_match(&regex, "x"));
+    }
+
+    #[test]
+    fn test_zero_or_one() {
+        let regex = Regex::from_str("ab?").unwrap();
+        assert!(is_match(&regex, "ac"));
+        assert!(is_match(&regex, "ab"));
+    }
+
+    #[test]
+    fn test_zero_or_more() {
+        let regex = Regex::from_str("ab*").unwrap();
+        assert!(is_match(&regex, "a"));
+        assert!(is_match(&regex, "abc"));
+        assert!(is_match(&regex, "abbbbb"));
+    }
+
+    #[test]
+    fn test_wildcard() {
+        let regex = Regex::from_str(".").unwrap();
+        assert!(is_match(&regex, "a"));
+        assert!(is_match(&regex, "1"));
+        assert!(is_match(&regex, " "));
+        assert!(is_match(&regex, "$!"));
+        assert!(!is_match(&regex, ""));
+    }
+
+    #[test]
+    fn test_alternation() {
+        let regex = Regex::from_str("(a|b)").unwrap();
+        assert!(is_match(&regex, "a"));
+        assert!(is_match(&regex, "b"));
+        assert!(!is_match(&regex, "c"));
+    }
+
+    #[test]
+    fn test_back_reference() {
+        let regex = Regex::from_str("(a)\\1").unwrap();
+        assert!(is_match(&regex, "aa"));
+        assert!(!is_match(&regex, "ab"));
+
+        let regex = Regex::from_str("(\\w)\\1").unwrap();
+        assert!(is_match(&regex, "aa"));
+        assert!(is_match(&regex, "bb"));
+        assert!(!is_match(&regex, "ab"));
+
+        let regex = Regex::from_str("a(\\w\\w)\\1").unwrap();
+        assert!(is_match(&regex, "aabab"));
+        assert!(is_match(&regex, "ababa"));
+
+        let regex = Regex::from_str("^I see (\\d (cat|dog|cow)(, | and )?)+$").unwrap();
+        assert!(!is_match(&regex, "I see 1 cat, 2 dogs and 3 cows"));
+        let regex = Regex::from_str(r"\d\\d\\dx").unwrap();
+        assert!(!is_match(&regex, "12x"));
+
+        let regex = Regex::from_str("^(cat) n \\1$").unwrap();
+        assert!(is_match(&regex, "cat n cat"));
+    }
+
+    #[test]
+    fn test_utf8() {
+        let regex = Regex::from_str("🦀").unwrap();
+        assert!(is_match(&regex, "🦀"));
+        assert!(!is_match(&regex, "🦁"));
+    }
+
+    #[test]
+    fn nested_back_ref() {
+        let regex = Regex::from_str("((c)\\2)\\1").unwrap();
+        assert!(is_match(&regex, "cccc"));
+    }
+}
